98f943163d05cee2194462ae79be42f8cc3751a0
alem2coder1 <148032941+alem2coder1@users.noreply.github.com>
2024-04-29 17:20:23 +0500

    fuultext

--- js/mian.js
+++ js/mian.js
@@ -25,3 +25,15 @@ document.addEventListener("DOMContentLoaded", function () {
         element.textContent = text;
     });
 });
+window.addEventListener('scroll', function () {
+    var footer = document.querySelector('footer'); // 获取 footer 元素
+    var fulltextLeftCard = document.querySelector('.fulltext-left-card'); // 获取具有 fulltext-left-card 类的卡片元素
+
+    var footerTop = footer.getBoundingClientRect().top; // 获取 footer 元素顶部相对于视口的位置
+
+    if (footerTop < window.innerHeight) { // 如果 footer 已经进入视口
+        fulltextLeftCard.classList.remove('fixed-right'); // 移除固定样式
+    } else {
+        fulltextLeftCard.classList.add('fixed-right'); // 否则添加固定样式
+    }
+});
