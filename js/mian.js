35f34beee4abcf51f033b4cd37abd01b95771e1c
alem2coder1 <148032941+alem2coder1@users.noreply.github.com>
2024-04-30 14:51:55 +0500

    about

--- js/mian.js
+++ js/mian.js
@@ -19,8 +19,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     maxTitle.forEach(element => {
         let text = element.textContent.trim();
-        if (text.length > 200) {
-            text = text.substring(0, 200) + '...';
+        if (text.length > 250) {
+            text = text.substring(0, 250) + '...';
         }
         element.textContent = text;
     });
@@ -40,16 +40,57 @@ document.addEventListener("DOMContentLoaded", function () {
             icon.classList.toggle("fa-solid");
         }
     });
-});
-window.addEventListener('scroll', function () {
-    var footer = document.querySelector('footer'); // 获取 footer 元素
-    var fulltextLeftCard = document.querySelector('.fulltext-left-card'); // 获取具有 fulltext-left-card 类的卡片元素
-
-    var footerTop = footer.getBoundingClientRect().top; // 获取 footer 元素顶部相对于视口的位置
-
-    if (footerTop < window.innerHeight) { // 如果 footer 已经进入视口
-        fulltextLeftCard.classList.remove('fixed-right'); // 移除固定样式
-    } else {
-        fulltextLeftCard.classList.add('fixed-right'); // 否则添加固定样式
+    let tagSwiper = null;
+    const initTagSwiper = function () {
+        if (this.window.outerWidth < 992) {
+            tagSwiper = new Swiper(".partner-swiper", {
+                slidesPerView: 3,
+                spaceBetween: 32,
+                freeMode: true,
+            });
+        } else {
+            tagSwiper = new Swiper(".partner-swiper", {
+                slidesPerView: 10,
+                spaceBetween: 20,
+                freeMode: true,
+            });
+        }
+    };
+    window.addEventListener("resize", function () {
+        initTagSwiper();
     }
+
+    );
+    initTagSwiper();
+
+    window.addEventListener('scroll', function () {
+        var footer = document.querySelector('footer');
+        var fulltextLeftCard = document.querySelector('.fulltext-left-card');
+
+        if (footer && fulltextLeftCard) {
+            var footerTop = footer.getBoundingClientRect().top;
+
+            if (footerTop < window.innerHeight) {
+                fulltextLeftCard.classList.remove('fixed-right');
+            } else {
+                fulltextLeftCard.classList.add('fixed-right');
+            }
+        }
+    });
 });
+$(function () {
+    $("#pagination").pagination({
+        items: 43,
+        itemsOnPage: 3,
+        currentPage: 1,
+        displayedPages: 3,
+        prevText: 'Назад',
+        nextText: 'Вперед',
+        displayedPages: $(window).width() < 768 ? 1 : 3,
+        onPageClick: function (pageNumber, event) {
+            //  // @* window.location.href = "/@language/article/list?@Html.Raw(catagoryId > 0 ? $"cId = { catagoryId } & " : "")@Html.Raw(tagId > 0 ? $"tagId = { tagId } & " : "")@Html.Raw(!string.IsNullOrEmpty(keyWord) ? $"keyWord = { keyWord } & " : "")page=" + pageNumber; * @
+        }
+    });
+});
+
+
